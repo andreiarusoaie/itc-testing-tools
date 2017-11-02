843a2bb2df04af59ff2cfa4033cef179007782d0
Andrei Arusoaie <andrei.arusoaie@gmail.com>
2017-11-02 17:02:59 +0200

    Added flawfinder

--- python/flawfinder-parser.py
+++ python/flawfinder-parser.py
@@ -1,12 +1,9 @@
 import re
+import sys
 import os
-import time
-
-import system
-
 
 # this is supposed to match over 'filename:line:column:warning message' format
-extractor = re.compile(b'(.+?\.[ch])\:([0-9]+?):(.+?)$')
+extractor = re.compile(b'(.+?\.[ch])\:([0-9]+?)\:(.+?)$')
 
 # traverse the lines, and consider only those having the above format
 # the rest is ignored for now
@@ -15,17 +12,17 @@ with open(sys.argv[1]) as f:
 
 lno = 0
 for line in lines:
-    matched = extractor.match(line)
+    matched = extractor.match(str.encode(line))
     if (not matched == None):
         filename = os.path.basename(matched.group(1).decode("utf-8"))
         line_no = matched.group(2).decode("utf-8")
         key = filename + ":" + line_no
-        error_msg = matched.group(3).decode("utf-8")
+        error_msg = matched.group(3).decode("utf-8").strip()
         k = 1
-        while (lines[lno + k].strip() and (extractor.match(lines[lno+k]) == None)):
-            error_msg = error_msg + str(lines[lno+k].decode("utf-8"))
+        while (lines[lno + k].strip() and (extractor.match(str.encode(lines[lno+k])) == None)):
+            error_msg = error_msg + str(lines[lno+k]).strip()
             k = k + 1
-        print(filename, line_no, error_msg)
+        print(filename, ",", line_no, ",", error_msg, end="")
      
     lno = lno + 1
 
