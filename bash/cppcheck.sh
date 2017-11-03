b5308b679ec31beadc7f0ec09140f44840b16995
Stefan Ciobaca <stefan.ciobaca@gmail.com>
2017-11-03 19:58:10 +0200

    Fixed small issues with cppcheck.

--- bash/cppcheck.sh
+++ bash/cppcheck.sh
@@ -3,13 +3,19 @@ CSV=$2
 EXE=$3
 OPTS=$4
 
+echo "CALLING CPPCHECK.SH"
+echo "DIR = ${DIR}"
+echo "CSV = ${CSV}"
+echo "EXE = ${EXE}"
+echo "OPTS = ${OPTS}"
+
 PARSER=../python/cppcheck-parser.py
 echo "File, Line, Error" > $CSV
 
 echo "$DIR\n$CSV\n$EXE\n$OPTS"
 
-echo $($EXE --quiet $DIR $OPTS 2> cppcheck-output.xml)
+$EXE --quiet $DIR $OPTS 2> cppcheck-output.xml
 XML=$(pwd)
 XML="$XML/cppcheck-output.xml"
-echo $(python3 $PARSER $XML >> $CSV)
+python3 $PARSER $XML >> $CSV
 rm $XML
