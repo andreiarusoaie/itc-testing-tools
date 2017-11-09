8efaae85522d5d5f8f2422b9c13b6187e652e309
Andrei Arusoaie <andrei.arusoaie@gmail.com>
2017-11-09 09:05:16 +0200

    Infer bash becomes infer python.

---
+++ python/infer.py
@@ -0,0 +1,35 @@
+import json
+import sys
+import os.path
+import system
+import dirutils
+import tempfile
+
+directory = os.path.abspath(sys.argv[1])
+csv       = os.path.abspath(sys.argv[2])
+exe       = sys.argv[3]
+opts      = sys.argv[4]
+
+print("======Running Infer=======")
+print("Working dir:", directory)
+print("CSV file:", csv)
+print("Excutable:", exe)
+print("Executable options:", opts)
+
+(output, err, exit, time) = system.system_call("make clean", directory)
+print(err)
+print(output)
+(output, err, exit, time) = system.system_call(exe + " run -- make", directory)
+print(err)
+print(output)
+
+sys.stdout = open(csv, "w")
+print("File, Line, Error")
+report_file = os.path.join(directory, "infer-out", "report.json")
+if (os.path.exists(report_file)):
+    with open(report_file) as json_report_file:
+        data = json.load(json_report_file)
+    for d in data:
+        print(d['file'].strip(), ",", str(d['line']), ",", "\"" + d['qualifier'] + "\"")
+sys.stdout = sys.__stdout__
+print("======Done with Infer=======")
