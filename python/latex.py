22e2bf3c0267c6b55054517fd14d05d3e50b571e
Andrei Arusoaie <andrei.arusoaie@gmail.com>
2017-11-17 15:34:43 +0200

    Update total table.

--- python/latex.py
+++ python/latex.py
@@ -17,7 +17,7 @@ def nice(toolname):
     if toolname == 'framac':
         return "Frama-C"
     if toolname == "clanalyze":
-        return "cl -analyze"
+        return "MSVC"
     return toolname.capitalize()
     
 def total(tex_file_name, rep_directory, tool_list):
@@ -59,7 +59,7 @@ def total(tex_file_name, rep_directory, tool_list):
     sys.stdout = open(tex_file_path, "w")
     print("\\begin{tabular}{|l|r|r|r|r|r|r|}")
     print("\\hline")
-    print("Tool & DR & FPR & PR & RDR & Unique & Time (s) \\\\ ")
+    print("\multicolumn{1}{|c|}{Tool} & \multicolumn{1}{|c|}{DR} & \multicolumn{1}{|c|}{FPR} & \multicolumn{1}{|c|}{PR} & \multicolumn{1}{|c|}{RDR} & \multicolumn{1}{|c|}{U} & \multicolumn{1}{|c|}{Time} \\\\ ")
     print("\\hline")
     for t in srt:
         t_as_list = list(map(lambda x : "{:4.2f}".format(x) if isinstance(x, float) else str(x), list(t)))
@@ -75,22 +75,40 @@ def total(tex_file_name, rep_directory, tool_list):
 def defects_dr(tex_file_name, rep_directory, tool_list):
     tex_file_path = os.path.join(rep_directory, tex_file_name)
 
+    t_map = {}
+    defects = set()
     for tool in tool_list:
         c_total_path = os.path.join(rep_directory, tool, 'c_defects.csv')
         head, *tail = lines(c_total_path)
-        # cpp_total_path = os.path.join(rep_directory, tool, 'cpp_defects.csv')
-        # h, *t = lines(cpp_total_path)
-        
-        for line in tail:
+        cpp_total_path = os.path.join(rep_directory, tool, 'cpp_defects.csv')
+        h, *t = lines(cpp_total_path)
+
+        def_map = {}
+        for line in tail + t:
             items = line.split(",")
             name = items[0]
-            dr = items[4]
-            
-            print(tool, name, dr)
+            defects.add(name)
+            def_map[name] = (0, 0)
+
+            tp  = int(items[1].strip())
+            var = int(items[3].strip())
+            dr  = round((tp * 100) / var, 2)
+            def_map[name] = (def_map[name][0] + tp, def_map[name][1] + var)
+            print(tool, name, dr, def_map[name])
+        t_map[tool] = def_map
 
 
-        print("\n")
+    print(t_map)
+    print(defects)
         
-#        name  = items[0].strip()
-#        print(name)
+    # sys.stdout = open(tex_file_path, "w")
+    # print("\\begin{tabular}{|l|r|r|r|r|r|r|}")
+    # print("\\hline")
+    # for defect in sorted(defects):
+    #     print(defect, "&")
+    # print("\\hline")
     
+    # print("\\hline")
+    # print("\\end{tabular}")
+    # sys.stdout = sys.__stdout__
+
