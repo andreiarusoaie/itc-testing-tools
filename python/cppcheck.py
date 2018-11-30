dc1ed3779a4a3a943c5a1fb5cdca535f9af99af8
Andrei Arusoaie <andrei.arusoaie@gmail.com>
2018-11-30 16:27:33 +0000

    Cppcheck: execute once per file; added temporary cwd; revised output in csv; revised output messages.

--- python/cppcheck.py
+++ python/cppcheck.py
@@ -4,30 +4,44 @@ import system
 import dirutils
 import tempfile
 import xml.etree.ElementTree as ET
+import shutil
 
-temp_path = os.path.abspath(sys.argv[1])
-directory = os.path.abspath(sys.argv[2])
-csv       = os.path.abspath(sys.argv[3])
-exe       = sys.argv[4]
-opts      = sys.argv[5]
+xml_report_path = os.path.abspath(sys.argv[1])
+directory       = os.path.abspath(sys.argv[2])
+csv             = os.path.abspath(sys.argv[3])
+exe             = sys.argv[4]
+opts            = sys.argv[5]
 
-print("======Running cppcheck=======")
-print("Working dir:", directory)
-print("CSV file:", csv)
-print("Excutable:", exe)
-print("Executable options:", opts)
+# create temporary dir to run the analyzer
+tmpdir_path = os.path.join("/home","itc","tmp", next(tempfile._get_candidate_names()))
+shutil.copytree(directory, tmpdir_path)
 
-(output, err, exit, time) = system.system_call(exe + " --quiet " + opts + " " + directory + " --output-file=" + temp_path, ".")
+print("\n======[CPPCHECK]=======")
+print("[CWD]:", tmpdir_path)
+print("[CSV]:", csv)
+print("[EXE]:", exe)
+print("[EXE OPTIONS]:", opts)
 
-report=temp_path
-sys.stdout = open(csv, "w")
+source_files = dirutils.list_files(tmpdir_path, '.c') + dirutils.list_files(tmpdir_path, '.cpp')
+if os.path.exists(csv):
+    os.remove(csv)
+sys.stdout = open(csv, "a")
 print("File, Line, Error")
-tree = ET.parse(report)
-root = tree.getroot()
-errors = root[1]
-for error in errors:
-    msg = "\"" + error.attrib['verbose'] + "\""
-    for location in error:
-        print(os.path.basename(location.attrib['file']) + ",", location.attrib['line'] + ",", msg)
 sys.stdout = sys.__stdout__
-print("====== Done with cppcheck=======")
+
+for source_file in source_files:
+    cppcheck = exe + opts + " " + source_file + " --output-file=" + xml_report_path
+    (output, err, exit, time) = system.system_call(cppcheck, ".")
+    tree = ET.parse(xml_report_path)
+    root = tree.getroot()
+    errors = root[1]
+    sys.stdout = open(csv, "a")
+    for error in errors:
+        msg = "\"" + error.attrib['verbose'] + "\""
+        for location in error:
+            print(os.path.basename(location.attrib['file']) + ",", location.attrib['line'] + ",", msg)
+    sys.stdout = sys.__stdout__
+
+print("[CLEANUP]: removing ", tmpdir_path)
+shutil.rmtree(tmpdir_path)    
+print("======[DONE WITH CPPCHECK]=======")
