80e04a7a3ce8f92a9cb25b66c4a5d36c9766a698
Andrei Arusoaie <andrei.arusoaie@gmail.com>
2018-12-01 19:45:30 +0000

    Updated oclint.

--- python/oclint.py
+++ python/oclint.py
@@ -3,6 +3,7 @@ import os.path
 import system
 import dirutils
 import tempfile
+import shutil
 
 temp_path = os.path.abspath(sys.argv[1])
 directory = os.path.abspath(sys.argv[2])
@@ -13,24 +14,32 @@ if (len(sys.argv) > 5):
 else:
     opts = ""
 
-print("======Running oclint=======")
-print("Working dir:", directory)
-print("CSV file:", csv)
-print("Excutable:", exe)
-print("Executable options:", opts)
+# create temporary dir to run the analyzer
+tmpdir_path = os.path.join("/home","itc","tmp", "oclint-" + next(tempfile._get_candidate_names()))
+shutil.copytree(directory, tmpdir_path)
+    
+print("======[OCLINT]=======")
+print("[CWD]:", tmpdir_path)
+print("[CSV]:", csv)
+print("[EXE]:", exe)
+print("[EXE OPTIONS]:", opts)
 
-c_files = dirutils.list_files(directory, '.c') + dirutils.list_files(directory, '.cpp')
-(output, err, exit, time) = system.system_call(exe + " " + " ".join(c_files) + " " + opts, directory)
+source_files = dirutils.list_files(tmpdir_path, '.c') + dirutils.list_files(tmpdir_path, '.cpp')
 
-temp_file = open(temp_path, 'w')
-temp_file.write(output.decode("utf-8"))
-temp_file.close()
-
+if os.path.exists(csv):
+    os.remove(csv)
 sys.stdout = open(csv, "w")
 print("File, Line, Error")
-with open(temp_path) as f:
-    for line in f.readlines():
-        a = line.strip().split(":")
+sys.stdout = sys.__stdout__
+
+
+for source_file in source_files:
+    oclint = exe + " " + source_file
+    (output, err, exit, time) = system.system_call(oclint, tmpdir_path)
+    lines = output.splitlines()
+    sys.stdout = open(csv, "a")
+    for line in lines:
+        a = line.decode("utf-8").strip().split(":")
         if (len(a) >= 3):
             message = a[3]
             i = 4
@@ -38,6 +47,8 @@ with open(temp_path) as f:
                 message = message + ":" + a[i]
                 i = i + 1
             print(os.path.basename(a[0]), ",", a[1], ",", "\"" + message + "\"")
+    sys.stdout = sys.__stdout__
 
-sys.stdout = sys.__stdout__            
-print("======Done with oclint=======")
+print("[CLEANUP]: removing ", tmpdir_path)
+shutil.rmtree(tmpdir_path)
+print("======[DONE WITH OCLINT]=======")
