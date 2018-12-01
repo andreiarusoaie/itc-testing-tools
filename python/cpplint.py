72e5a298983493bc07449332d23971b46df97375
Andrei Arusoaie <andrei.arusoaie@gmail.com>
2018-12-01 19:56:48 +0000

    Updated cpplint.

--- python/cpplint.py
+++ python/cpplint.py
@@ -3,6 +3,7 @@ import os.path
 import system
 import dirutils
 import tempfile
+import shutil
 
 temp_path = os.path.abspath(sys.argv[1])
 directory = os.path.abspath(sys.argv[2])
@@ -13,24 +14,30 @@ if (len(sys.argv) > 5):
 else:
     opts = ""
 
-print("======Running cpplint=======")
-print("Working dir:", directory)
-print("CSV file:", csv)
-print("Excutable:", exe)
-print("Executable options:", opts)
+# create temporary dir to run the analyzer
+tmpdir_path = os.path.join("/home","itc","tmp", "cpplint-" + next(tempfile._get_candidate_names()))
+shutil.copytree(directory, tmpdir_path)
+    
+print("======[CPPLINT]=======")
+print("[CWD]:", tmpdir_path)
+print("[CSV]:", csv)
+print("[EXE]:", exe)
+print("[EXE OPTIONS]:", opts)
 
-c_files = dirutils.list_files(directory, '.c') + dirutils.list_files(directory, '.cpp')
-(output, err, exit, time) = system.system_call(exe + " " + " ".join(c_files) + " " + opts, directory)
-
-temp_file = open(temp_path, 'w')
-temp_file.write(err.decode("utf-8"))
-temp_file.close()
-
+source_files = dirutils.list_files(tmpdir_path, '.c') + dirutils.list_files(tmpdir_path, '.cpp')
+if os.path.exists(csv):
+    os.remove(csv)
 sys.stdout = open(csv, "w")
 print("File, Line, Error")
-with open(temp_path) as f:
-    for line in f.readlines():
-        a = line.strip().split(":")
+sys.stdout = sys.__stdout__
+
+for source_file in source_files:
+    cpplint = exe + " " + source_file
+    (output, err, exit, time) = system.system_call(cpplint, tmpdir_path)
+    sys.stdout = open(csv, "a")
+    lines = err.splitlines()
+    for line in lines:
+        a = line.decode("utf-8").strip().split(":")
         if (len(a) >= 3):
             message = a[2]
             # i = 4
@@ -38,6 +45,8 @@ with open(temp_path) as f:
             #     message = message + ":" + a[i]
             #     i = i + 1
             print(os.path.basename(a[0]), ",", a[1], ",", message)
+    sys.stdout = sys.__stdout__
 
-sys.stdout = sys.__stdout__            
-print("======Done with cpplint=======")
+print("[CLEANUP]: removing ", tmpdir_path)
+shutil.rmtree(tmpdir_path) 
+print("======[DONE WITH CPPLINT]=======")
