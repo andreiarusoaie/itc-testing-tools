416fa176b8b07207259336b4156df2ad937086eb
Andrei Arusoaie <andrei.arusoaie@gmail.com>
2018-12-01 19:45:50 +0000

    Updated flint++.

--- python/flint++.py
+++ python/flint++.py
@@ -3,6 +3,8 @@ import sys
 import os.path
 import system
 import dirutils
+import shutil
+import tempfile
 
 temp_path = os.path.abspath(sys.argv[1])
 directory = os.path.abspath(sys.argv[2])
@@ -10,27 +12,34 @@ csv       = os.path.abspath(sys.argv[3])
 exe       = sys.argv[4]
 opts      = sys.argv[5]
 
-print("======Running flint++=======")
-print("Working dir:", directory)
-print("CSV file:", csv)
-print("Excutable:", exe)
-print("Options:", opts)
+# create temporary dir to run the analyzer
+tmpdir_path = os.path.join("/home","itc","tmp", "flintpp-" + next(tempfile._get_candidate_names()))
+shutil.copytree(directory, tmpdir_path)
 
-c_files = dirutils.list_files(directory, '.c') + dirutils.list_files(directory, '.cpp')
-(output, err, exit, time) = system.system_call(exe + " " + opts + " " + " ".join(c_files), directory)
+print("======[FLINT++]=======")
+print("[CWD]:", tmpdir_path)
+print("[CSV]:", csv)
+print("[EXE]:", exe)
+print("[EXE OPTIONS]:", opts)
 
-temp_file = open(temp_path, 'w')
-temp_file.write(output.decode("utf-8"))
-temp_file.close()
-
+source_files = dirutils.list_files(tmpdir_path, '.c') + dirutils.list_files(tmpdir_path, '.cpp')
+if os.path.exists(csv):
+    os.remove(csv)
 sys.stdout = open(csv, "w")
 print("File, Line, Error")
-if (os.path.exists(temp_path)):
-    with open(temp_path) as json_report_file:
-        data = json.load(json_report_file)
+sys.stdout = sys.__stdout__
+
+for source_file in source_files:
+    flintpp = exe + " " + opts + " " + source_file
+    (output, err, exit, time) = system.system_call(flintpp, tmpdir_path)
+    data = json.loads(output.decode("utf-8"))
+    sys.stdout = open(csv, "a")
     for f in data['files']:
         filename = f['path']
         for error in f['reports']:
-            print(filename, ",", error['line'], ",", error['title'])
-sys.stdout = sys.__stdout__
-print("======Done with flint++=======")
+            print(os.path.basename(filename), ",", error['line'], ",", error['title'])
+    sys.stdout = sys.__stdout__
+    
+print("[CLEANUP]: removing ", tmpdir_path)
+shutil.rmtree(tmpdir_path)  
+print("======[DONE WITH FLINT++]=======")
