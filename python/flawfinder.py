be1b83a3fa393a98dc2966b7f30e5da8babede92
Andrei Arusoaie <andrei.arusoaie@gmail.com>
2018-12-13 09:26:20 +0000

    Fixed output parsing for flawfinder.

--- python/flawfinder.py
+++ python/flawfinder.py
@@ -30,18 +30,28 @@ sys.stdout = sys.__stdout__
 for source_file in source_files:
     flawfinder = exe + " " + source_file
     (output, err, exit, time) = system.system_call(flawfinder, tmpdir_path)
-    lines = output.splitlines()
+    all_lines = output.splitlines()
+    
+    lines = []
+    line_codes = []
+    collect_flag = False
+    for line in all_lines:
+        dec = line.decode("utf-8").strip()
+        if (collect_flag):
+            lines.append(dec)
+            if (len(dec.split(":")) >= 3):
+                line_codes.append(True)
+            else:
+                line_codes.append(False)
+        if dec == "FINAL RESULTS:":
+            collect_flag = True
+        if dec == "ANALYSIS SUMMARY:":
+            break
+    
     sys.stdout = open(csv, "a")
-    collectingMode = False
-    error_message = "unknown"
-    filename = "unknown"
-    line_no = "unknown"
-    for line in lines:
-        a = line.decode("utf-8").strip().split(":")
-        if (len(a) >= 3):
-            if (collectingMode):
-                print(filename, ",", line_no, ",", "\"" + error_message + "\"")
-                collectingMode = False # this is needed to disable final
+    for i in range(0,len(lines)):
+        if (line_codes[i]):
+            a = lines[i].split(":")
             filename = os.path.basename(a[0])
             line_no = a[1]
             error_message = ""
@@ -49,10 +59,14 @@ for source_file in source_files:
             while (j < len(a)):
                 error_message = error_message + ":" + a[j]
                 j = j + 1
-            collectingMode = True
-        else:
-            if (collectingMode):
-                error_message = error_message + line.decode("utf-8").strip()
+            j = i + 1
+            while (j < len(lines)):
+                if (not line_codes[j]):
+                    error_message += error_message + " " + lines[j].strip()
+                    j = j + 1
+                else:
+                    break;
+            print(filename, ",", line_no, ",", "\"" + error_message + "\"")
     sys.stdout = sys.__stdout__            
 
 print("[CLEANUP]: removing ", tmpdir_path)
