c36e6b72a4d85fa14a533b4d8dc3d83027ec8c1b
Andrei Arusoaie <andrei.arusoaie@gmail.com>
2018-12-14 11:40:29 +0000

    Updated cpplint log.

--- python/cpplint.py
+++ python/cpplint.py
@@ -26,25 +26,25 @@ print("[EXE]:", exe)
 print("[EXE OPTIONS]:", opts)
 
 source_files = dirutils.list_files(tmpdir_path, '.c') + dirutils.list_files(tmpdir_path, '.cpp')
-if os.path.exists(csv):
-    os.remove(csv)
-sys.stdout = open(csv, "w")
-print("File, Line, Error")
-sys.stdout = sys.__stdout__
+dirutils.file_line_error_header(csv)
+dirutils.reset_file(temp_path)
 
 for source_file in source_files:
+    if source_file.endswith("main.c"):
+        continue
+    if source_file.endswith("invalid_extern_1.c"):
+        continue
+    if source_file.endswith("invalid_extern.c"):
+        source_file = source_file + " " + os.path.join(tmpdir_path, "invalid_extern_1.c")
     cpplint = exe + " " + source_file
     (output, err, exit, time) = system.system_call(cpplint, tmpdir_path)
+    dirutils.tool_exec_log(temp_path, cpplint, output, err, exit)
     sys.stdout = open(csv, "a")
     lines = err.splitlines()
     for line in lines:
         a = line.decode("utf-8").strip().split(":")
         if (len(a) >= 3):
             message = a[2]
-            # i = 4
-            # while (i < len(a)):
-            #     message = message + ":" + a[i]
-            #     i = i + 1
             print(os.path.basename(a[0]), ",", a[1], ",", message)
     sys.stdout = sys.__stdout__
 
