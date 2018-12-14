e40c911a3f329d23e66530ac4e6473aeaf8d8edb
Andrei Arusoaie <andrei.arusoaie@gmail.com>
2018-12-14 12:14:28 +0000

    Updated sparse.

--- python/sparse.py
+++ python/sparse.py
@@ -34,16 +34,20 @@ if directory.endswith("Cpp") :
 hfile2 = os.path.join(tmpdir_path, "HeaderFile.hx")
 copyfile(hfile2, hfile1);
 
-if os.path.exists(csv):
-    os.remove(csv)
-sys.stdout = open(csv, "w")
-print("File, Line, Error")
-sys.stdout = sys.__stdout__
+dirutils.file_line_error_header(csv)
+dirutils.reset_file(temp_path)
 
 source_files = dirutils.list_files(tmpdir_path, '.c') + dirutils.list_files(tmpdir_path, '.cpp')
 for source_file in source_files:
+    if source_file.endswith("main.c"):
+        continue
+    if source_file.endswith("invalid_extern_1.c"):
+        continue
+    if source_file.endswith("invalid_extern.c"):
+        source_file = source_file + " " + os.path.join(tmpdir_path, "invalid_extern_1.c")
     sparse = exe + " " + source_file + " " + opts;
     (output, err, exit, time) = system.system_call(sparse, tmpdir_path)
+    dirutils.tool_exec_log(temp_path, sparse, output, err, exit)
     lines = err.splitlines();
     sys.stdout = open(csv, "a")
     for line in lines:
