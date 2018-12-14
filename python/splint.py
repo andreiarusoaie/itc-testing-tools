dd693f2cdd84e78aa36b80993da079a2d75ae0d7
Andrei Arusoaie <andrei.arusoaie@gmail.com>
2018-12-14 12:18:28 +0000

    Updating splint.

--- python/splint.py
+++ python/splint.py
@@ -26,15 +26,20 @@ print("[EXE]:", exe)
 print("[EXE OPTIONS]:", opts)
 
 source_files = dirutils.list_files(tmpdir_path, '.c') + dirutils.list_files(tmpdir_path, '.cpp')
-if os.path.exists(csv):
-    os.remove(csv)
-sys.stdout = open(csv, "w")
-print("File, Line, Error")
-sys.stdout = sys.__stdout__
+dirutils.file_line_error_header(csv)
+dirutils.reset_file(temp_path)
+
 
 for source_file in source_files:
+    if source_file.endswith("main.c"):
+        continue
+    if source_file.endswith("invalid_extern_1.c"):
+        continue
+    if source_file.endswith("invalid_extern.c"):
+        source_file = source_file + " " + os.path.join(tmpdir_path, "invalid_extern_1.c")
     splint = exe + " -nestcomment +posixlib " + source_file + " " + opts
     (output, err, exit, time) = system.system_call(splint, tmpdir_path)
+    dirutils.tool_exec_log(temp_path, splint, output, err, exit)
     lines = output.splitlines()
     sys.stdout = open(csv, "a")
     for line in lines:
